c87d7e7dad4713fe771d4e00a6b67bb5905abed7
imamoonakhter <imamoonakhter@gmail.com>
2025-04-21 21:34:24 +0500

    design product page and pincode api

---
+++ pages/api/pincode.js
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    res.status(200).json([
+      74000, // Karachi
+      54000, // Lahore
+      44000, // Islamabad
+      46000, // Rawalpindi
+      38000, // Faisalabad
+      60000, // Multan
+      25000, // Peshawar
+      87300, // Quetta
+      51040, // Sialkot
+      52250, // Gujranwala
+      63100, // Bahawalpur
+      71000, // Hyderabad
+      22010, // Abbottabad
+      65200, // Sukkur
+      40100, // Sargodha
+      47150  // Murree
+    ]);
+  }
+  
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,10 +1,27 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { set } from "nprogress";
 import React from "react";
 
 const Slug = () => {
   const router = useRouter();
   const { slug } = router.query;
+  const [pin, setPin] = React.useState()
+  const [service, setService] = React.useState(null)
+  const checkServiceAbility = async () => {
+    let pins = await fetch('http://localhost:3000/api/pincode')
+    let pinJson = await pins.json()
+    if(pinJson.includes(parseInt(pin))) {
+      setService(true)
+    }
+    else {
+      setService(false)
+    }
+  }
+
+  const onChangePin = (e)=>{
+    setPin(e.target.value)
+  }
   return (
     <>
       <section className="text-gray-600 body-font overflow-hidden">
@@ -21,7 +38,7 @@ const Slug = () => {
   </div>
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                BRAND NAME
+                SANDHU CODESWEAR
               </h2>
               <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
                 The Catcher in the Rye
@@ -168,7 +185,10 @@ const Slug = () => {
                 <span className="title-font font-medium text-2xl text-gray-900">
                   $58.00
                 </span>
-                <button className="flex ml-14 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">
+                <button className="flex ml-8 text-white bg-pink-500 border-0 py-2 md:px-6 px-2 focus:outline-none hover:bg-pink-600 rounded">
+                  Buy Now
+                </button>
+                <button className="flex ml-4 text-white bg-pink-500 border-0 py-2 px-2 md:px-6 focus:outline-none hover:bg-pink-600 rounded">
                   Add to Cart
                 </button>
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
@@ -184,6 +204,17 @@ const Slug = () => {
                   </svg>
                 </button>
               </div>
+              <div className="pin mt-6 flex space-x-2 text-sm">
+                <input onChange={onChangePin} className="px-2 border-2 border-gray-400 rounded-md" type="text" placeholder="Enter your Pincode" name="" id="" />
+                <button  className="text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded" onClick={checkServiceAbility}>Check</button>
+              </div>
+              {!service && service != null && <div className="text-red-700 text-sm mt-3">
+                Sorry! We do not deliver to this pincode yet
+              </div>}
+
+              {service && service != null && <div className="text-green-700 text-sm mt-3">
+                Yay! This pincode is serviceable
+              </div>}
             </div>
           </div>
         </div>
