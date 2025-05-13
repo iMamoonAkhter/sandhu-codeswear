19394fc30da1b3d7c60bb6deda5cb8d11d6975a9
imamoonakhter <imamoonakhter@gmail.com>
2025-05-13 23:46:05 +0500

    Some text remove from mug and hoodie

--- pages/hoodies.js
+++ pages/hoodies.js
@@ -9,7 +9,6 @@ const Hoodies = ({ product }) => {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4 justify-center">
-            Products comment
             {product && product.products.map((item) => {
               console.log("Link", item.category, item.slug)
               return <div key={item._id} className="lg:w-1/4 md:w-1/2 p-4 w-full">
